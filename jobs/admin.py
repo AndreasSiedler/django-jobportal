6d4d4764284ec5ecfcac4ba146eb0d68d7af73aa
Andreas Siedler <andreassiedler@andreass-air.home>
2020-05-21 23:49:36 +0200

    added autocomplete

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,28 +1,37 @@
 from django.contrib import admin
-from .models import Job, Title, Skill, Skillship, Location, Category  
-
+from .models import Job, Title, Skill, Skillship, Location, Category
 
 
 class SkillshipInline(admin.TabularInline):
-    model = Skillship
-    extra = 1 # how many rows to show
+    model                   = Skillship
+    extra                   = 1 # how many rows to show
+    autocomplete_fields     = ['skill']
+
+
 
 class JobAdmin(admin.ModelAdmin):
-    inlines = (SkillshipInline,)
-    list_filter = ('title__title', 'title',)
-    search_fields = ('title__title', 'title',)
-    fields = ('title', 'location', 'tasks', 'offers', 'company',)
+    inlines                 = (SkillshipInline,)
+    list_filter             = ('title__title', 'title',)
+    search_fields           = ('title__title', 'title',)
+    fields                  = ('title', 'location', 'tasks', 'offers', 'company',)
+    autocomplete_fields     = ['title', 'location',]
 
     def save_model(self, request, obj, form, change):
         if getattr(obj, 'user', None) is None:
             obj.user = request.user
         obj.save()
 
+
 class TitleAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
+    search_fields       = ('title',)
+
+class SkillAdmin(admin.ModelAdmin):
+    search_fields       = ('name',)
 
 class LocationAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
+    search_fields       = ('title',)
 
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
@@ -31,6 +40,6 @@ class CategoryAdmin(admin.ModelAdmin):
 # Register your models here.
 admin.site.register(Job, JobAdmin)
 admin.site.register(Title, TitleAdmin)
-admin.site.register(Skill)
+admin.site.register(Skill, SkillAdmin)
 admin.site.register(Category, CategoryAdmin) 
 admin.site.register(Location, LocationAdmin) 
--- jobs/forms.py
+++ jobs/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from jobs.models import Job, Skill, Applicant
+from .widgets.widgets import DynamicArrayWidget
 
 # class AddSkillForm(forms.ModelForm):
 #     class Meta:
@@ -11,6 +12,51 @@ from jobs.models import Job, Skill, Applicant
 #         self.fields["skills"].widget = forms.widgets.CheckboxSelectMultiple()
 #         self.fields["skills"].help_text = ""
 #         self.fields["skills"].queryset = Skill.objects.all()
+
+
+# class DynamicArrayField(forms.Field):
+
+#     default_error_messages = {
+#         'item_invalid': 'Item %(nth)s in the array did not validate: ',
+#     }
+
+#     def __init__(self, base_field, **kwargs):
+#         self.get = False
+#         self.attrs = False
+#         self.is_hidden = False
+#         self.needs_multipart_form = False
+#         self.base_field = base_field
+#         self.max_length = kwargs.pop('max_length', None)
+#         kwargs.setdefault('widget', DynamicArrayWidget)
+#         super().__init__(**kwargs)
+
+#     def clean(self, value):
+#         cleaned_data = []
+#         errors = []
+#         value = filter(None, value)
+#         for index, item in enumerate(value):
+#             try:
+#                 cleaned_data.append(self.base_field.clean(item))
+#             except forms.ValidationError as error:
+#                 errors.append(prefix_validation_error(
+#                     error, self.error_messages['item_invalid'],
+#                     code='item_invalid', params={'nth': index},
+#                 ))
+#         if errors:
+#             raise forms.ValidationError(list(chain.from_iterable(errors)))
+#         if cleaned_data and self.required:
+#             raise forms.ValidationError(self.error_messages['required'])
+#         return cleaned_data
+
+
+# class JobAdminForm(forms.ModelForm):
+#     class Meta:
+#         model = Job
+#         widgets = {
+#         'tasks': DynamicArrayField(models.CharField(max_length=300)),
+#         }
+#         fields = '__all__'
+
 
 
 class CreateJobForm(forms.ModelForm):
