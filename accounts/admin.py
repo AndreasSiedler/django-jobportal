5472b4613713dcd52c6f3a4a5cc640191674a899
Andreas Siedler <andreassiedler@Andreass-MacBook-Air.local>
2020-05-31 23:19:53 +0200

    imporved resgistration

--- accounts/admin.py
+++ accounts/admin.py
@@ -5,21 +5,30 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Group
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-User = get_user_model()
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import User
+
 
 class UserAdmin(BaseUserAdmin):
-    list_display = ('email', 'is_superuser')
-    list_filter = ('is_superuser',)
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = User
+    list_display = ('email', 'is_staff', 'is_active',)
+    list_filter = ('email', 'is_staff', 'is_active',)
     fieldsets = (
         (None, {'fields': ('email', 'password',)}),
         ('Personal info', {'fields': ('first_name','last_name',)}),
         ('Profiles', {'fields': ('is_employee', 'is_employer',)}),
         ('Permissions', {'fields': ('is_superuser', 'is_staff', 'is_active',)}),
     )
+    add_fieldsets = (
+        (None, {'fields': ('email', 'password1', 'password2',)}),
+        ('Personal info', {'fields': ('first_name','last_name',)}),
+        ('Profiles', {'fields': ('is_employee', 'is_employer',)}),
+        ('Permissions', {'fields': ('is_superuser', 'is_staff', 'is_active',)}),
+    )
     search_fields = ('email',)
     ordering = ('email',)
-    filter_horizontal = ()
-
 
 admin.site.register(User, UserAdmin)
 
